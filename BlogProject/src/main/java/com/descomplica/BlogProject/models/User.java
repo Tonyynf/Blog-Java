4dd334450558a634874dc137c125fd0a10c99b47
Tonyynf <tobyencarnacao@gmail.com>
2025-11-03 18:28:08 -0300

    Adicionando dados, contrutores, getters e setters ao model "User" e Post

--- BlogProject/src/main/java/com/descomplica/BlogProject/models/User.java
+++ BlogProject/src/main/java/com/descomplica/BlogProject/models/User.java
@@ -1,4 +1,68 @@
 package com.descomplica.BlogProject.models;
 
+import com.descomplica.BlogProject.enums.RoleEnum;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "User")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long UserId;
+    private String name;
+    private String email;
+    private String password;
+    private RoleEnum role;
+
+    public User(final Long userId, final String name, final String email, final String password, final RoleEnum role) {
+        UserId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User() {
+
+    }
+
+    public Long getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(Long userId) {
+        UserId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public RoleEnum getRole() {
+        return role;
+    }
+
+    public void setRole(RoleEnum role) {
+        this.role = role;
+    }
 }
