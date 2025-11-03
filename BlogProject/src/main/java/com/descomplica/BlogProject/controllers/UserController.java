f9c6c5e0f86257651460f1142e3519ad040ed142
Tonyynf <tobyencarnacao@gmail.com>
2025-11-03 19:28:55 -0300

    Incluindo função de salvar usuario

--- BlogProject/src/main/java/com/descomplica/BlogProject/controllers/UserController.java
+++ BlogProject/src/main/java/com/descomplica/BlogProject/controllers/UserController.java
@@ -1,4 +1,18 @@
 package com.descomplica.BlogProject.controllers;
 
+import com.descomplica.BlogProject.models.User;
+import com.descomplica.BlogProject.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/users")
+
 public class UserController {
+    private UserService userService;
+
+    @PostMapping("/save")
+    private @ResponseBody User save(@RequestBody User user){
+        return userService.save(user);
+    }
 }
--- BlogProject/src/main/java/com/descomplica/BlogProject/services/UserService.java
+++ BlogProject/src/main/java/com/descomplica/BlogProject/services/UserService.java
@@ -1,4 +1,7 @@
 package com.descomplica.BlogProject.services;
 
+import com.descomplica.BlogProject.models.User;
+
 public interface UserService {
+    User save(User user);
 }
