433feda1b4f10912d932423f7b38cca9d63e9ce0
Tonyynf <tobyencarnacao@gmail.com>
2025-11-04 17:04:58 -0300

    Implementando o CRUD

--- BlogProject/src/main/java/com/descomplica/BlogProject/services/UserService.java
+++ BlogProject/src/main/java/com/descomplica/BlogProject/services/UserService.java
@@ -2,6 +2,16 @@ package com.descomplica.BlogProject.services;
 
 import com.descomplica.BlogProject.models.User;
 
+import java.util.List;
+
 public interface UserService {
     User save(User user);
+
+    User get(Long id);
+
+    List<User> getAll();
+
+    User update(Long id, User user);
+
+    void delete(Long id);
 }
